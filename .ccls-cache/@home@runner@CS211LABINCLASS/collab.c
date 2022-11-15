62b00df1f0aa6253cc32d1d8c0f186cb2319bec8
KathanParikh1 <>
2022-11-15 16:25:30 +0000

    first modified version of the function

---
+++ .ccls-cache/@home@runner@CS211LABINCLASS/collab.c
@@ -0,0 +1,38 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void thabin2_kpari3() {
+  printf("Dory Habing and Kathan Parikh\n");
+  printf("In order to commit and push files to the cloud github, you\n");
+  printf("have to get the personal access token through the developer settings\n");
+  printf("From there you can generate a new access token, which would substitute the password");
+}
+
+int main() {
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+    thabin2_kpari3();
+  
+    return 0;
+}
